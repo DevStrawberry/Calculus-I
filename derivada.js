178f704796bf428795ac7c417e1463a60624983c
DevStrawberry <matheustrindade05@outlook.com>
2025-04-30 22:02:31 -0300

    Add files via upload

---
+++ derivada.js
@@ -0,0 +1,64 @@
+const prompt = require("prompt-sync")();
+
+let n = Number(prompt("Digite a quantidade de termos: "));
+
+function lerValores() {
+    let coeficiente = Number(prompt("Digite o coeficiente: "));
+    let expoente = Number(prompt("Digite o expoente: "));
+    return [coeficiente, expoente];
+}
+
+let funcao = [];
+for (let i = 0; i < n; i++) {
+    funcao.push(lerValores());
+}
+
+console.log("\nFunção original:");
+for (let i = 0; i < n; i++) {
+    console.log(`Termo ${i + 1}: coef = ${funcao[i][0]}, exp = ${funcao[i][1]}`); 
+}
+
+// Derivada de potencia
+for (let i = 0; i < n; i++) {
+    if (funcao[i][1] != 0) {
+        let exp = funcao[i][1];
+        funcao[i][1] = funcao[i][1] - 1;
+        funcao[i][0] = exp * funcao[i][0];
+    } else {
+        funcao[i][0] = 0;
+        funcao[i][1] = 0;
+    }
+}
+
+console.log("\nPrimeira derivada da função:");
+for (let i = 0; i < n; i++) {
+    console.log(`Termo ${i + 1}: coef = ${funcao[i][0]}, exp = ${funcao[i][1]}`); 
+}
+
+// Derivada de potencia
+for (let i = 0; i < n; i++) {
+    if (funcao[i][1] !== 0) {
+        let exp = funcao[i][1];
+        funcao[i][1] = funcao[i][1] - 1;
+        funcao[i][0] = exp * funcao[i][0];
+    } else {
+        funcao[i][0] = 0;
+        funcao[i][1] = 0;
+    }
+}
+
+let ptoCritico = null;
+if (n === 1) { // Se a função tiver apenas um termo 
+    let exp = funcao[0][1];
+    if (exp === 0) {
+        ptoCritico = "Todos os valores de x (função constante)";
+    } else if (exp > 0) {
+        ptoCritico = 0;
+    }
+}
+console.log(`\nPonto crítico: ${ptoCritico}`);
+
+console.log("\nSegunda derivada da função:");
+for (let i = 0; i < n; i++) {
+    console.log(`Termo ${i + 1}: coef = ${funcao[i][0]}, exp = ${funcao[i][1]}`); 
+}
